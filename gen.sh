3eef42330cb1c409d6bc5d82c12cc2b21ccb3c58
Gerard de Leeuw <gdeleeuw@leeuwit.nl>
2023-02-01 07:58:25 +0100

    Generate Go files

--- gen.sh
+++ gen.sh
@@ -2,10 +2,15 @@
 protoc \
 		-I. \
 		--go_out=. \
+		--go_opt=paths=source_relative \
 		--go-grpc_out=. \
-		--openapiv2_out=. \
+		--go-grpc_opt=paths=source_relative \
 		--grpc-gateway_out=. \
+		--grpc-gateway_opt logtostderr=true \
+		--grpc-gateway_opt paths=source_relative \
 		--grpc-gateway_opt generate_unbound_methods=true \
+		--openapiv2_out=. \
+		--openapiv2_opt logtostderr=true \
 		project/project.proto \
 		cluster/cluster.proto \
 		vm/vm.proto
