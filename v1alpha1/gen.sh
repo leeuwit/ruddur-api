2b60aad6a9426102dede483ba17d0ad8c9c7e534
Gerard de Leeuw <gdeleeuw@leeuwit.nl>
2023-03-07 20:53:27 +0100

    Formatting

--- v1alpha1/gen.sh
+++ v1alpha1/gen.sh
@@ -3,17 +3,16 @@ set -e
 export SCRIPT_FILE=$(readlink -f "$0")
 export SCRIPT_DIR=$(dirname "${SCRIPT_FILE}")
 cd "${SCRIPT_DIR}"
-protoc \
-		-I. \
-		-Ivendor \
-		--go_out=. \
-		--go_opt=paths=source_relative \
-		--go-grpc_out=. \
-		--go-grpc_opt=paths=source_relative \
-		--grpc-gateway_out=. \
-		--grpc-gateway_opt logtostderr=true \
-		--grpc-gateway_opt paths=source_relative \
-		--grpc-gateway_opt generate_unbound_methods=true \
-		--openapiv2_out=. \
-		--openapiv2_opt logtostderr=true \
-		ruddur.proto
+protoc -I. \
+  -Ivendor \
+  --go_out=. \
+  --go_opt=paths=source_relative \
+  --go-grpc_out=. \
+  --go-grpc_opt=paths=source_relative \
+  --grpc-gateway_out=. \
+  --grpc-gateway_opt logtostderr=true \
+  --grpc-gateway_opt paths=source_relative \
+  --grpc-gateway_opt generate_unbound_methods=true \
+  --openapiv2_out=. \
+  --openapiv2_opt logtostderr=true \
+  ruddur.proto
